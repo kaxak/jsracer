98ae76c2d345151858d843f33f9778ac7b7a6ccf
Emmanuel ROCHE <emmanuel.roche@rochedev.fr>
2016-04-18 04:59:11 +0200

    X.Input doc

--- js/lib/tool/Input.js
+++ js/lib/tool/Input.js
@@ -2,7 +2,7 @@
 Created : 23/02/2016
 Authors : GINOT Gilles, ROCHE Emmanuel
 Description :
-    Classe servant à definir les evenement inputs.
+    Récupère les entrées des périphériques clavier, souris et aussis des gamepads.
 Aide :
     event keypress : https://developer.mozilla.org/fr/docs/Web/Events/keypress
     connaitre le keyCode d'une touche : http://keycode.info/
@@ -13,43 +13,86 @@ Aide :
 X.Input = new function() {
     
     /* Properties */
-    //objet littéral contenant les keycode en provenance de l'utilisateur
+    /**
+     * Contient les entrées clavier
+     * @type Array
+     */
     var _keys = [];
+    /**
+     * Contient les entrées souris
+     * @type Array
+     */
     var _mouseButtons = [];
+    /**
+     * Contient les entrées des gamepads
+     * @type Array
+     */
     var _gamepadButtons = [];
     
-    //attibut et enleve les keycode de l'objet KeysDown en fonction
-    //des touches du clavier utilisées
+    /**
+     * Gère le clavier lorsqu'une touche est enfoncée
+     */
     window.addEventListener("keydown", function (e) {
         _preventDefault(e);
         _keys[e.keyCode] = true;
     }, false);
-
+    /**
+     * Gère le clavier lorsqu'une touche est relâchée
+     */
     window.addEventListener("keyup", function (e) {
         _preventDefault(e);
         delete _keys[e.keyCode];
     }, false);
     
     /**
-     * Retourne 'true' si la touche correspondante a "key" est pressée.
-     * @param {string} key : touche, keyCode ou string faisant référence à une touche du clavier (touche:'a' ou keyCode:65)
+     * Bloque l’action par défaut de la plus part des touches sauf celles contenue 
+     * dans le tableau excepts de la méthode (ctrl, F5, F11)
+     * @param {event} e évènnement concerné
      */
-    this.isPressed = function(keys){
-        if(typeof keys === 'object' || keys instanceof Object){
+    var _preventDefault = function(e){
+        if(e.keyCode){
+            var excepts = [17,116,122];
+            if(!excepts.includes(e.keyCode)){
+                e.preventDefault();
+            }
+        }
+    };
+    
+    /**
+     * Test toutes les entrées contenue dans l'argument binds 
+     * jusqu'à se qu'il en trouve un vrai qu'il retournera 
+     * sinon la méthode retourne faux car aucune de ces entrées 
+     * est activées.
+     * Exemple d'un bind :
+     * var mesBinds = {
+     *      keyboard:['d','right',...],
+     *      gamepads:{
+     *          0:['DRIGHT',...]
+     *      },
+     *      mouse:[...]
+     *  };
+     * @param {object} binds
+     */
+    this.isPressed = function(binds){
+        if(typeof binds === 'object' || binds instanceof Object){
             var key = null;
             //* Test les entrées clavier
-            for(var i = 0; i < keys.keyboard.length; ++i){
-                key = keys.keyboard[i];
-                if(typeof key === 'string' || key instanceof String){
-                    if(_getKeyCode(key.toUpperCase()) in _keys) return true;
+            if(binds.keyboard){
+                for(var i = 0; i < binds.keyboard.length; ++i){
+                    key = binds.keyboard[i];
+                    if(typeof key === 'string' || key instanceof String){
+                        if(_getKeyCode(key.toUpperCase()) in _keys) return true;
+                    }
                 }
             }
             //* Test les entrées des gamepads
-            for(var id in keys.gamepads){
-                for(var i = 0; i < keys.gamepads[id].length; ++i){
-                    key = keys.gamepads[id][i];
-                    if(typeof key === 'string' || key instanceof String){
-                        if(_getX360Code(key.toUpperCase()) in _gamepadButtons[id]) return true;
+            if(binds.gamepads){
+                for(var id in binds.gamepads){
+                    for(var i = 0; i < binds.gamepads[id].length; ++i){
+                        key = binds.gamepads[id][i];
+                        if(typeof key === 'string' || key instanceof String){
+                            if(_getX360Code(key.toUpperCase()) in _gamepadButtons[id]) return true;
+                        }
                     }
                 }
             }
@@ -59,8 +102,7 @@ X.Input = new function() {
     };
     
     /**
-     * 
-     * @returns {undefined}
+     * Stocke l'état des gamepads de manière simplifié.
      */
     this.pollGamepads = function(){
         var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : null);
@@ -87,8 +129,9 @@ X.Input = new function() {
     
     //* http://stackoverflow.com/questions/94037/convert-character-to-ascii-code-in-javascript
     /**
-     * Retourne le keyCode (integer) d'une touche ou 'false' si la "key" ne correspond à rien.
-     * @param {string} key : touche, keyCode ou string faisant référence à une touche du clavier (touche:'a' ou keyCode:65)
+     * Récupère le code correspondant à une touche.
+     * @param {string} key : nom de la touche du clavier.
+     * @returns {Number|Boolean} le code de la touche du clavier ou faux en cas d'échec.
      */
     var _getKeyCode = function(key){
         if(typeof key === 'string' || key instanceof String){
@@ -117,22 +160,17 @@ X.Input = new function() {
                     return 40;
             }
         }
-        else if(typeof key === 'number'){
-            if(key % 1 === 0){
-                return key;
-            }
-        }
         return false;
     };
     
     /**
-     * 
-     * @param {type} btn
-     * @returns {Number|Boolean}
+     * Récupère le code correspondant à une button d'un gamepad Xinput.
+     * @param {string} key : nom du bouton du gamepad.
+     * @returns {Number|Boolean} le code de la touche du clavier ou faux en cas d'échec.
      */
-    var _getX360Code = function(btn) {
-        if(typeof btn === 'string' || btn instanceof String){
-            switch (btn) {
+    var _getX360Code = function(key) {
+        if(typeof key === 'string' || key instanceof String){
+            switch (key) {
                 case 'A':
                     return 0;
                     break;
@@ -174,24 +212,11 @@ X.Input = new function() {
                     return 15;
             }
         }
-        else if(typeof btn === 'number'){
-            if(btn >= 0 && btn <= 15) return btn;
+        else if(typeof key === 'number'){
+            if(key >= 0 && key <= 15) return key;
         }
         return false;
     };
     
-    /**
-     * 
-     * @param {type} e
-     * @returns {undefined}
-     */
-    var _preventDefault = function(e){
-        if(e.keyCode){
-            var excepts = [17,116,122];
-            if(!excepts.includes(e.keyCode)){
-                e.preventDefault();
-            }
-        }
-    };
     
 }();
